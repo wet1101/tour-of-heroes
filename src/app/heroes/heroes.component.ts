bb02370996ef1508409882a4a79a9facab6f8a9a
Edward Lee <Edward@Lees-MacBook-Air.local>
2019-01-23 17:25:38 +0800

    first app

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
